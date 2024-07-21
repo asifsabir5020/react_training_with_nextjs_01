6632f8ca89fd97c4ca823d4c0401d18a29b745f5
asifsabir5020 <asif.sabir.5020@gmail.com>
2024-07-21 09:37:48 +0500

    pg added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,23 @@
+import { Pool } from 'pg'
 import styles from "./page.module.css";
 
-export default function Home() {
+const dbConnectionPool = async () => {
+  const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL
+  })
+  const createTableText = `
+  CREATE TABLE tech_languages(
+    title VARCHAR (50) UNIQUE NOT NULL
+  );
+`
+  const response = await pool.query(createTableText)
+  return response
+}
+
+
+export default async function Home() {
+  const data = await dbConnectionPool()
+  console.log('data:#:', data)
   return (
     <main className={styles.main}>
       Welcome to React & Nextjs
