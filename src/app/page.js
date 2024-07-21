bd26f77316c7b3619c7c2f69beec6718a0295ada
asifsabir5020 <asif.sabir.5020@gmail.com>
2024-07-21 09:47:33 +0500

    std form init

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,6 @@
-import { Pool } from 'pg'
 import styles from "./page.module.css";
 
-const dbConnectionPool = async () => {
-  const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL
-  })
-  const createTableText = `
-  CREATE TABLE tech_languages(
-    title VARCHAR (50) UNIQUE NOT NULL
-  );
-`
-  const response = await pool.query(createTableText)
-  return response
-}
-
-
 export default async function Home() {
-  const data = await dbConnectionPool()
-  console.log('data:#:', data)
   return (
     <main className={styles.main}>
       Welcome to React & Nextjs
