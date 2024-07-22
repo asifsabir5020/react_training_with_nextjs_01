459a87a8a0b4eb44668d84fa5e1d8c22d83a7b94
asifsabir5020 <asif.sabir.5020@gmail.com>
2024-07-22 09:35:10 +0500

    academic section init

--- src/app/components/student-admission-form/index.jsx
+++ src/app/components/student-admission-form/index.jsx
@@ -1,42 +1,87 @@
 'use client'
 
+import { classes, grades } from '@/constants'
 import styles from './styles.module.css'
+
 
 export const StudentAdmissionForm = () => {
     return (
         <div className={styles.container}>
             <form>
-                <div className={styles.personalInfoSection}>
-                    <div className={styles.imageSection}>
-                        <img src="student-image.jpg" className={styles.image} />
+                <div className={styles.formContent}>
+                    <div className={styles.personalInfoSection}>
+                        <div className={styles.imageSection}>
+                            <img src="student-image.jpg" className={styles.image} />
+                        </div>
+                        <div className={styles.infoSection}>
+                            <div className={styles.formField}>
+                                <label className={styles.fieldLabel}>
+                                    Name
+                                </label>
+                                <input
+                                    className={styles.inputField}
+                                    placeholder='Enter Student Name'
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label className={styles.fieldLabel}>
+                                    Father Name
+                                </label>
+                                <input
+                                    className={styles.inputField}
+                                    placeholder='Enter Father Name'
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label className={styles.fieldLabel}>
+                                    B-Form
+                                </label>
+                                <input
+                                    className={styles.inputField}
+                                    placeholder='Enter B-Form'
+                                />
+                            </div>
+                        </div>
                     </div>
-                    <div className={styles.infoSection}>
+                    <div className={styles.academicInfoSection}>
                         <div className={styles.formField}>
                             <label className={styles.fieldLabel}>
-                                Name
+                                Last Class
                             </label>
-                            <input
-                                className={styles.inputField}
-                                placeholder='Enter Student Name'
-                            />
+                            <select className={styles.selectField}>
+                                <option key="000" disabled>Select Class</option>
+                                {classes.map(c => {
+                                    return (
+                                        <option key={c.id}>{c.title}</option>
+                                    )
+                                })}
+                            </select>
                         </div>
                         <div className={styles.formField}>
                             <label className={styles.fieldLabel}>
-                                Father Name
+                                Last Grade
                             </label>
-                            <input
-                                className={styles.inputField}
-                                placeholder='Enter Father Name'
-                            />
+                            <select className={styles.selectField}>
+                                <option key="000" disabled>Select Grade</option>
+                                {grades.map(g => {
+                                    return (
+                                        <option key={g.id}>{g.title}</option>
+                                    )
+                                })}
+                            </select>
                         </div>
                         <div className={styles.formField}>
                             <label className={styles.fieldLabel}>
-                                B-Form
+                                Admission Class
                             </label>
-                            <input
-                                className={styles.inputField}
-                                placeholder='Enter B-Form'
-                            />
+                            <select className={styles.selectField}>
+                                <option key="000" disabled>Select Grade</option>
+                                {classes.map(g => {
+                                    return (
+                                        <option key={g.id}>{g.title}</option>
+                                    )
+                                })}
+                            </select>
                         </div>
                     </div>
                 </div>
---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const classes = [
+    { id: 1, title: '5th', value: 5 },
+    { id: 2, title: '6th', value: 6 },
+    { id: 3, title: '7th', value: 7 },
+    { id: 4, title: '8th', value: 8 },
+    { id: 5, title: '9th', value: 9 },
+    { id: 1, title: '10th', value: 10 },
+]
+
+export const grades = [
+    { id: 1, title: 'A' },
+    { id: 2, title: 'B' },
+    { id: 3, title: 'C' },
+    { id: 4, title: 'F' },
+]
